4dcb899d35fd6dd6e3fe48a7f0ca27cd7e8a94de
David J Crockett <crockett.davidj@gmail.com>
2022-12-21 18:32:05 -0800

    Set up routes for database

--- server-side/app.js
+++ server-side/app.js
@@ -12,6 +12,50 @@ const PORT = process.env.PORT || 3002;
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on Port ${PORT}`)
+// Route to view all favorited video games
+app.get("/api/get", (req,res)=>{
+  db.query("SELECT * FROM favorites", (err,result) => {
+      if(err) {
+        console.log(err)
+      } res.send(result)
   });
+});
+  
+// Route to view one video game
+app.get("/api/getFromId/:id", (req,res)=>{
+  const id = req.params.id;
+  
+  db.query("SELECT * FROM favorites WHERE id = ?", id, (err,result) => {
+    if(err) {
+      console.log(err)
+    } res.send(result)
+  });
+});
+
+// Route for adding a favorite video game
+app.post('/api/create', (req,res) => {
+  const title = req.body.title;
+  const text = req.body.text;
+
+  db.query("INSERT INTO favorites (title, post_text) VALUES (?,?,?)",[title,text], (err,result)=>{
+    if(err) {
+      console.log(err)
+    } console.log(result)
+  });   
+})
+
+// Route to deleting a game from favorites
+
+app.delete('/api/delete/:id',(req,res) => {
+  const id = req.params.id;
+  
+  db.query("DELETE FROM favorites WHERE id= ?", id, (err,result)=>{
+    if(err) {
+      console.log(err)
+    } 
+  }) 
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on Port ${PORT}`)
+});
