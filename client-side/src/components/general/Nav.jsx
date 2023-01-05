2618924bd8e108d75cadc1da9efebc168ba1a11f
David J Crockett <crockett.davidj@gmail.com>
2023-01-04 16:31:11 -0800

    minor edits

--- client-side/src/components/general/Nav.jsx
+++ client-side/src/components/general/Nav.jsx
@@ -4,9 +4,16 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
 function NavComponent() {
+
+    const styles = {
+		brand: {
+		  padding: '0rem 0 0 3rem'
+		}
+	  }
+
     return (
         <Navbar bg='dark' variant='dark'>
-            <Navbar.Brand as={Link} to='/'>Find Your Adventure</Navbar.Brand>
+            <Navbar.Brand as={Link} to='/' style={styles.brand}>Find Your Adventure</Navbar.Brand>
             <Navbar.Toggle aria-controls = 'basic-navbar-nav' />
 
                 <Nav>
